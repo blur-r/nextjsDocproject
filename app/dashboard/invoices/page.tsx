042a81f5637bd5d02be205df9155d03d584026c9
Babalola Paul <pbabalola27@gmail.com>
2025-10-17 13:23:26 +0100

    start of chapter 12 plus the prerendering bug fix

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,45 @@
+// import Pagination from '@/app/ui/invoices/pagination';
+// import Search from '@/app/ui/search';
+// import Table from '@/app/ui/invoices/table';
+// import { CreateInvoice } from '@/app/ui/invoices/buttons';
+// import { lusitana } from '@/app/ui/fonts';
+// import { Suspense } from 'react';
+// import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+// import { fetchInvoicesPages } from '@/app/lib/data';
+
+
+// export default async function Page(props: {
+//     searchParams?: Promise<{
+//         query?: string;
+//         page?: string;
+//     }>;
+// }) {
+//     const searchParams = await props.searchParams;
+//     const query = searchParams?.query || '';
+//     const currentPage = Number(searchParams?.page) || 1;
+//     const totalPages = await fetchInvoicesPages(query);
+
+//     return (
+//         <div className="w-full">
+//             <div className="flex w-full items-center justify-between">
+//                 <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+//             </div>
+//             <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+//                 <Search placeholder="Search invoices..." />
+//                 <CreateInvoice />
+//             </div>
+//             <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+//                 <Table query={query} currentPage={currentPage} />
+//             </Suspense>
+//             <div className="mt-5 flex w-full justify-center">
+//                 <Suspense fallback={<p>Loading...</p>}>
+//                     <Pagination totalPages={totalPages} />
+//                 </Suspense>
+//             </div>
+//         </div>
+//     );
+// }
+
 import Pagination from '@/app/ui/invoices/pagination';
 import Search from '@/app/ui/search';
 import Table from '@/app/ui/invoices/table';
@@ -6,40 +48,52 @@ import { lusitana } from '@/app/ui/fonts';
 import { Suspense } from 'react';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { fetchInvoicesPages } from '@/app/lib/data';
-// import TotalPages from './TotalPages';
 
-
-export default async function Page(props: {
+export default function Page({
+    searchParams,
+}: {
     searchParams?: Promise<{
         query?: string;
         page?: string;
     }>;
 }) {
-    const searchParams = await props.searchParams;
-    const query = searchParams?.query || '';
-    const currentPage = Number(searchParams?.page) || 1;
-    const totalPages = await fetchInvoicesPages(query);
-
     return (
         <div className="w-full">
             <div className="flex w-full items-center justify-between">
                 <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
             </div>
             <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-                <Search placeholder="Search invoices..." />
+                <Suspense fallback={<p>Loading search...</p>}>
+                    <Search placeholder="Search invoices..." />
+                </Suspense>
                 <CreateInvoice />
             </div>
-            <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
-                <Table query={query} currentPage={currentPage} />
+            <Suspense fallback={<InvoicesTableSkeleton />}>
+                <DynamicPart searchParams={searchParams} />
             </Suspense>
-            <div className="mt-5 flex w-full justify-center">
-                <Suspense fallback={<p>Loading...</p>}>
-                    <Pagination totalPages={totalPages} />
-                </Suspense>
-                {/* <Suspense fallback={<p>Loading...</p>}>
-                    <TotalPages query={query} />
-                </Suspense> */}
-            </div>
         </div>
+    );
+}
+
+async function DynamicPart({
+    searchParams,
+}: {
+    searchParams?: Promise<{
+        query?: string;
+        page?: string;
+    }>;
+}) {
+    const params = await searchParams || {};
+    const query = params?.query || '';
+    const currentPage = Number(params?.page) || 1;
+    const totalPages = await fetchInvoicesPages(query);
+
+    return (
+        <>
+            <Table query={query} currentPage={currentPage} />
+            <div className="mt-5 flex w-full justify-center">
+                <Pagination totalPages={totalPages} />
+            </div>
+        </>
     );
 }
