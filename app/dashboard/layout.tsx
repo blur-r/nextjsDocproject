8a613e7486d7fd5b93026493e891ba24dfb69511
Babalola Paul <pbabalola27@gmail.com>
2025-10-17 16:51:49 +0100

    fixed prerender bug

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,11 +1,15 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
+import { Suspense } from 'react';
 export const experimental_ppr = true;
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
             <div className="w-full flex-none md:w-64">
-                <SideNav />
+                <Suspense fallback={<p>Loading nav...</p>}>
+                    <SideNav />
+                </Suspense>
+
             </div>
             <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
         </div>
